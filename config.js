48d9b3a7e1eaca142f5f2869b529f2a0c3ac00d6
Sandrine Pradier <labelleaffranchie@gmail.com>
2017-12-08 16:14:57 +0100

    views and menu ok

--- config.js
+++ config.js
@@ -1,14 +1,17 @@
 angular.module('weatherApp', ['ui.router']).config(function ($stateProvider){
 $stateProvider.state('currentWeather', {
           url: '/',
-          templateUrl: 'components/weather/main_view.html'
+          templateUrl: 'components/weather/main_view.html',
+          controller: 'appController'
         });
 $stateProvider.state('nextHoursWeather', {
           url: '/nextHoursWeather',
-          templateUrl: 'components/weather/nexthours_view.html'
+          templateUrl: 'components/weather/nexthours_view.html',
+          controller: 'appController'
         });
 $stateProvider.state('nextDaysWeather', {
           url: '/nextDaysWeather',
-          templateUrl: 'components/weather/nextdays_view.html'
+          templateUrl: 'components/weather/nextdays_view.html',
+          controller: 'appController'
         });
 });
